9fcc41858d8e74930b86ef8f2586af4a50aec4dd
Denver Madsen <denver@olympustech.com>
2022-03-01 14:13:20 -0500

    removes bisect package, calculates t-score from confidence level

--- velocity.ts
+++ velocity.ts
@@ -1,13 +1,42 @@
-import { std } from "mathjs";
+import { std, mean } from "mathjs";
 import * as dist from "@stdlib/stats-base-dists-t";
-import * as bisect from "bisect";
+
+/**
+ *
+ * @param value monotonic function
+ * @param expected expected output
+ * @param low lower bound input
+ * @param high upper bound input
+ * @param tolerance maximum error
+ * @returns
+ */
+function binarySearch(
+  value: (x: number) => number,
+  expected: number,
+  low: number,
+  high: number,
+  tolerance: number
+) {
+  while (high - low > tolerance * 2) {
+    const guess = mean(low, high);
+    console.log(guess);
+    if (value(guess) < expected) {
+      low = guess;
+    } else {
+      high = guess;
+    }
+  }
+  return mean(low, high);
+}
 
 const data = [1, 4, 7, 2, 6];
+const confidence = 0.95;
+const low = -10;
+const high = 10;
+const tolerance = 1e-8;
 const s = std(data, "uncorrected");
-const degreesFreedom = data.length - 1;
-const t = new dist.T(19);
-const prob = t.cdf(1.7291);
-const confLevel = 0.95;
-const numStds = bisect((x: number) => t.cdf(x) >= confLevel, -10, 10, 1e-8);
+const degreesFreedom = data.length;
+const t = new dist.T(15);
+const numStds = binarySearch((x) => t.cdf(x), confidence, low, high, tolerance);
 
 console.log(`# STDs: ${numStds}`);
