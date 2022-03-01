146ecc2a67cd798d4e94d37b9170599dbe7a011a
Denver Madsen <denver@olympustech.com>
2022-03-01 14:31:12 -0500

    takes arguments from CLI

--- velocity.ts
+++ velocity.ts
@@ -1,5 +1,6 @@
-import { std, mean, sqrt } from "mathjs";
+import { std, mean, sqrt, number } from "mathjs";
 import * as dist from "@stdlib/stats-base-dists-t";
+import { argv } from "process";
 
 /**
  *
@@ -19,7 +20,6 @@ function binarySearch(
 ) {
   while (high - low > tolerance * 2) {
     const guess = mean(low, high);
-    console.log(guess);
     if (value(guess) < expected) {
       low = guess;
     } else {
@@ -28,9 +28,8 @@ function binarySearch(
   }
   return mean(low, high);
 }
-
-const data = [1, 4, 7, 2, 6];
-const confidence = 0.95;
+const data = argv.slice(2, -2).map<number>((x) => number(x) as number);
+const confidence = number(argv[argv.length - 1]) as number;
 const low = -10;
 const high = 10;
 const tolerance = 1e-8;
@@ -41,4 +40,8 @@ const tValue = binarySearch((x) => t.cdf(x), confidence, low, high, tolerance);
 const standardError = s / sqrt(data.length);
 const total = tValue * standardError + mean(data);
 
-console.log(`result: ${total}`);
+console.log(
+  `There is a ${confidence * 100}% chance that we can complete ${total.toFixed(
+    0
+  )} Story Points`
+);
